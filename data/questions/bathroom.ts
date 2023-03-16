0b462b11ea8d6030b2dc62d8d7faad08ec8686f3
no822 <nolan.kim@cobosys.co.kr>
2023-03-16 14:03:48 +0900

    bathroom, kitchen 질문들 추가

--- data/questions/bathroom.ts
+++ data/questions/bathroom.ts
@@ -1,18 +1,13 @@
 import {yesOrNo} from "../types/questions";
 
-export const bathroomQuestions: Array<yesOrNo> = [
-    {cleaningId: 'kitchen1', text: '설거지거리가 있나요?', difficulty: 'easy'},
-    {cleaningId: 'kitchen2', text: '설거지용 수세미를 소독할까요?', difficulty: 'easy'},
-    {cleaningId: 'kitchen3', text: '싱크대를 청소할까요', difficulty: 'easy'},
-    {cleaningId: 'kitchen4', text: '바닥 청소를 하실건가요?', difficulty: 'hard'},
-    {cleaningId: 'kitchen5', text: '벽면청소를 하실건가요?', difficulty: 'hard'},
-    {cleaningId: 'kitchen6', text: '바닥을 닦으실 건가요?', difficulty: 'hard'},
-    {cleaningId: 'kitchen7', text: '전자레인지 청소를 할까요?', difficulty: 'hard'},
-    {cleaningId: 'kitchen8', text: '버릴 음식물 쓰레기가 있나요?', difficulty: 'hard'},
-    {cleaningId: 'kitchen9', text: '오븐이 있나요?', difficulty: 'very hard'},
-    {cleaningId: 'kitchen10', text: '가스레인지 청소를 할까요?', difficulty: 'very hard'},
-    {cleaningId: 'kitchen11', text: '식기세척기를 청소할까요?', difficulty: 'very hard'},
-    {cleaningId: 'kitchen12', text: '배수구를 청소할까요?', difficulty: 'very hard'},
-    {cleaningId: 'kitchen13', text: '냉장고 청소한지 오래됐나요?', difficulty: 'very hard'},
+export const bathroomquestions: Array<yesOrNo> = [
+    {cleaningId: 'bath2', text: '간단하게 배수구를 청소할까요?', difficulty: 'easy'},
+    {cleaningId: 'bath4', text: '바닥솔질을 하실건가요?', difficulty: 'easy', additionalCleaning: ['bath3']},
+    {cleaningId: 'bath5', text: '벽면솔질을 하실건가요?', difficulty: 'easy', additionalCleaning: ['bath3']},
+    {cleaningId: 'bath6', text: '천장 청소를 하실건가요?', difficulty: 'very hard', additionalCleaning: ['bath3']},
+    {cleaningId: 'bath7', text: '변기를 청소할까요?', difficulty: 'very hard', additionalCleaning: ['bath3']},
+    {cleaningId: 'bath8', text: '세면대를 청소하실건가요?', difficulty: 'very hard', additionalCleaning: ['bath3']},
+    {cleaningId: 'bath9', text: '배수구를 청소하실건가요?', difficulty: 'very hard'},
+    {cleaningId: 'bath10', text: '곰팡이가 많이 슬어있나요?', difficulty: 'very hard'},
 ];
 
