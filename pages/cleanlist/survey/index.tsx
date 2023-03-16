123e3e24382c5ad4258bfa0da07ceb110caaff42
no822 <nolan.kim@cobosys.co.kr>
2023-03-16 14:33:35 +0900

    fix: progress bar 간격 조정2

--- pages/cleanlist/survey/index.tsx
+++ pages/cleanlist/survey/index.tsx
@@ -7,7 +7,7 @@ import OneInMore from "../../../components/questions/OneInMore";
 const Index = () => {
     return (
         <Layout>
-            <progress className="progress fixed progress-primary w-4/5 w-full translate-x-1/2 right-1/2" value="40" max="100"></progress>
+            <progress className="progress absolute progress-primary w-1/2 w-full translate-x-1/2 right-1/2" value="40" max="100"></progress>
             <p className='fixed w-60 translate-x-1/2 right-1/2 top-28 text-center'>n개의 질문이 남았습니다.</p>
             <div className='h-full w-full flex justify-center items-center'>
                 <YesOrNo text={'방해되는 물건들을 정리할까요?'} difficulty={'hard'} cleaningId={'c1'} />
