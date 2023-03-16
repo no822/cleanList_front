27043ed2cce09f782649849d20c17343120110ad
no822 <nolan.kim@cobosys.co.kr>
2023-03-16 15:09:29 +0900

    '여러개 중에 하나 선택'하는 타입의 설문화면

--- components/questions/OneInMore.tsx
+++ components/questions/OneInMore.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import {oneInMore} from "../../data/types/questions";
 import Card from "../ui/Card";
+import Button from "../ui/Button";
 
 type propsType = oneInMore & {
 
 };
 
 const OneInMore = ({...props}: propsType) => {
-    console.log(props)
+    console.log(
+
+    )
     return (
         <Card className=''>
-
+            <h2 className='leading-7 text-lg font-bold break-keep text-center'>{props.text}</h2>
+            <div className={`
+                mt-5
+                h-80 overflow-y-scroll 
+                grid grid-cols-2 
+                place-content-start gap-4`}
+            >
+                {props.options.map((text, i) =>
+                    <Button
+                        className=' btn-lg h-20 w-full'
+                        key={i} clickHandler={() => console.log('btn click')}
+                    >
+                        {text}
+                    </Button>
+                )}
+            </div>
         </Card>
     );
 };
--- pages/cleanlist/survey/index.tsx
+++ pages/cleanlist/survey/index.tsx
@@ -10,14 +10,14 @@ const Index = () => {
             <progress className="progress absolute progress-primary w-4/5 w-full translate-x-1/2 right-1/2" value="40" max="100"></progress>
             <p className='fixed w-60 translate-x-1/2 right-1/2 top-28 text-center'>n개의 질문이 남았습니다.</p>
             <div className='h-full w-full flex justify-center items-center'>
-                <YesOrNo text={'방해되는 물건들을 정리할까요?'} difficulty={'hard'} cleaningId={'c1'} />
-                {/*<OneInMore*/}
-                {/*    text={'어느 구역을 청소하시겠어요'}*/}
-                {/*    difficulty={'very hard'}*/}
-                {/*    target={'area'}*/}
-                {/*    options={['주방', '욕실', '침실']}*/}
-                {/*    values={['kitchen', 'bathroom', 'bedroom']}*/}
-                {/*/>*/}
+                {/*<YesOrNo text={'방해되는 물건들을 정리할까요?'} difficulty={'hard'} cleaningId={'c1'} />*/}
+                <OneInMore
+                    text={'어느 구역을 청소하시겠어요'}
+                    difficulty={'very hard'}
+                    target={'area'}
+                    options={['주방', '욕실', '침실', '베란다', 'test', 'test', 'test', 'test', 'test']}
+                    values={['kitchen', 'bathroom', 'bedroom', 'veranda']}
+                />
             </div>
         </Layout>
     );
