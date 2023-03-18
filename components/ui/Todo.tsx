0df64063b86f73bc21209bba824d1d0cfcaca39c
no822 <nolan.kim@cobosys.co.kr>
2023-03-18 16:19:17 +0900

    TodoContainer 컴포넌트 추가

---
+++ components/ui/Todo.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {cleaning} from "../../data/types/cleanings";
+
+type propsType = {
+    todo: cleaning
+}
+
+const TodoContainer = ({todo}: propsType) => {
+    const [isChecked, setIsChecked] = useState<boolean>(false);
+
+    const toggleCheck = () => {
+        setIsChecked(prev => !prev);
+    }
+
+    return (
+        <div className="relative alert bg-sky-400 shadow-lg">
+            <div>
+                <input
+                    type="checkbox"
+                    onChange={toggleCheck}
+                    checked={isChecked}
+                    className="checkbox absolute top-1/2 left-4 -translate-y-1/2"
+                />
+                <span className={`${isChecked ? 'line-through text-gray-300' : ''} 
+                                text-2xl font-bold text-sky-50`}>
+                    {todo.desc}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default TodoContainer;
