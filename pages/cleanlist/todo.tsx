624b6f595a589b39d7b35eebffc9370752ec4c88
no822 <nolan.kim@cobosys.co.kr>
2023-03-18 17:25:39 +0900

    투두리스트 데이터 없는 경우 라우팅, 투두리스트 하단 패딩 추가

--- pages/cleanlist/todo.tsx
+++ pages/cleanlist/todo.tsx
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {useRouter} from "next/router";
 import TodoContainer from "../../components/ui/Todo";
 import {useAppSelector} from "../../store/hooks";
 import {selectCleanings} from "../../store/cleaningSlice";
 import RadialProgress from "../../components/ui/RadialProgress";
 
 const TodoList = () => {
+    const router = useRouter();
     const [progress, setProgress] = useState(0);
     const cleanings = useAppSelector(selectCleanings);
     const orderByPriority = [...cleanings]
         .sort((a, b) => a.priority - b.priority);
 
+    useEffect(() => {
+        if (orderByPriority.length === 0) {
+            router.replace('/cleanlist');
+            return;
+        }
+    }, [router]);
 
     console.log('redux values', cleanings, orderByPriority);
     // 1. priority 를 기준으로 오름차순 정렬 ✓
@@ -19,14 +27,15 @@ const TodoList = () => {
     //  - 추가, 수정(desc)의 경우는? -> 일단 보류
 
     return (
-        <div className='flex flex-col gap-2 w-full'>
-            <div className='flex justify-end'>
+        <div className='w-full'>
+            <div className='flex justify-end pb-4'>
                 <RadialProgress nextValue={progress}/>
             </div>
-            {orderByPriority.length > 0 &&
-                orderByPriority.map(cleaning =>
-                    <TodoContainer key={cleaning.id} todo={cleaning}/>)}
-            <div className='h-5 invisible'>padding</div>
+            <ul className='flex flex-col gap-2 w-full pb-4'>
+                {orderByPriority.length > 0 &&
+                    orderByPriority.map(cleaning =>
+                        <TodoContainer key={cleaning.id} todo={cleaning}/>)}
+            </ul>
         </div>
     );
 };
