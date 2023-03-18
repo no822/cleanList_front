631e730f27ab7965432877a50ac69cc20ef34b13
no822 <nolan.kim@cobosys.co.kr>
2023-03-18 16:19:46 +0900

    fix: 공통타입 질문 데이터 추가
    - 세탁 관련 질문들 난이도 수정

--- data/questions/common.ts
+++ data/questions/common.ts
@@ -1,9 +1,9 @@
 import {yesOrNo} from "../types/questions";
 
 const commonQuestionsData: Array<yesOrNo> = [
-    {cleaningId: 'c2', text: '세탁할 빨래가 있습니까?', additionalCleaning: ['c3', 'c4', 'c5'], difficulty: 'hard', area: 'common' },
-    {cleaningId: 'c3', text: '세탁이 끝난 빨래가 있습니까?', additionalCleaning: ['c4', 'c5'], difficulty: 'hard', area: 'common'},
-    {cleaningId: 'c4', text: '다 마른 빨래가 있습니까?', additionalCleaning: ['c5'], difficulty: 'hard', area: 'common'},
+    {cleaningId: 'c2', text: '세탁할 빨래가 있습니까?', additionalCleaning: ['c3', 'c4', 'c5'], difficulty: 'easy', area: 'common' },
+    {cleaningId: 'c3', text: '세탁이 끝난 빨래가 있습니까?', additionalCleaning: ['c4', 'c5'], difficulty: 'easy', area: 'common'},
+    {cleaningId: 'c4', text: '다 마른 빨래가 있습니까?', additionalCleaning: ['c5'], difficulty: 'easy', area: 'common'},
     {cleaningId: 'c6', text: '오랜만에 현관바닥 청소를 할까요?', difficulty: 'hard', area: 'common'},
     {cleaningId: 'c7', text: '청소 후 버려야할 쓰레기가 있습니까?', difficulty: 'hard', area: 'common'},
     {cleaningId: 'c8', text: '오늘이 분리수거 날인가요?', difficulty: 'hard', area: 'common'},
