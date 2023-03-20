2ebb81a6aed4734e9a5e183fb243efb9d63ce428
no822 <nolan.kim@cobosys.co.kr>
2023-03-20 17:31:10 +0900

    test: todoItem에서 터치이벤트가 시작되었을 경우 스크롤 안되는 버그 확인2

--- components/ui/Todo.tsx
+++ components/ui/Todo.tsx
@@ -46,7 +46,7 @@ const TodoContainer = ({id, todo, animate, onCheck, onDelete}: propsType) => {
         <div className={`${animate} todo-container relative alert bg-sky-400 drop-shadow-lg`}
               ref={setNodeRef} {...attributes}  style={itemStyle}
         >
-            <div className={`h-full text-center border border-red-500 
+            <div className={` h-full text-center border border-red-500 
                             flex justify-center items-center w-full cursor-grab`}
             >
                 <input
@@ -55,7 +55,7 @@ const TodoContainer = ({id, todo, animate, onCheck, onDelete}: propsType) => {
                     checked={isChecked}
                     className="checkbox absolute top-1/2 left-4 -translate-y-1/2"
                 />
-                <div {...listeners} className='flex justify-center items-center absolute left-10 h-8 w-8'><RxDragHandleDots2 /></div>
+                <div {...listeners} className='touch-none flex justify-center items-center absolute left-10 h-8 w-8'><RxDragHandleDots2 /></div>
                 <span className={`${isChecked ? 'line-through text-zinc-400 italic' : ''}
                                   text-2xl font-bold text-sky-50`}>
                     {todo.desc}
