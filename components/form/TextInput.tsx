3ab9ed9b39c6f1be364a074e5ed2aae4e029c542
no822 <nolan.kim@cobosys.co.kr>
2023-03-22 06:44:47 +0900

    로그인, 회원가입 유효성 검사

--- components/form/TextInput.tsx
+++ components/form/TextInput.tsx
@@ -5,15 +5,17 @@ type propsType = {
     inputType?: string;
     placeHolder: string;
     onChange: (e: ChangeEvent) => void;
+    validateMsg?: string;
 };
 
-const TextInput = ({labelTxt, inputType = 'text', placeHolder, onChange}: propsType) => {
+const TextInput = ({labelTxt, inputType = 'text', placeHolder, onChange, validateMsg = ''}: propsType) => {
     return (
-        <div className="form-control w-full max-w-xs">
+        <div className="relative form-control w-full max-w-xs">
             <label className="label">
                 <span className="label-text">{labelTxt}</span>
             </label>
             <input type={inputType} onChange={onChange} placeholder={placeHolder} className="input input-bordered w-full max-w-xs"/>
+            <div className='absolute top-20 pt-2 pl-2 text-red-500 bg-transparent'>{validateMsg}</div>
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, {ChangeEvent, useState, useEffect} from 'react';
 import type { NextPage } from 'next'
-import {AuthErrorCodes} from "@firebase/auth";
+import {AuthErrorCodes, createUserWithEmailAndPassword} from "@firebase/auth";
 import auth from "../network/auth";
 import InformModal from "../components/ui/InformModal";
 import TextInput from "../components/form/TextInput";
@@ -9,15 +9,20 @@ import {useAppDispatch} from "../store/hooks";
 import {authAction} from "../store/authSlice";
 import {signInWithEmailAndPassword} from "@firebase/auth";
 import {useRouter} from "next/router";
+import {validateEmail, validatePassword} from "../utils/inputValidate";
 
 
 const Home: NextPage = () => {
     const router = useRouter()
     const dispatch = useAppDispatch();
+    const [isLoginForm, setIsLoginForm] = useState<boolean>(true);
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [passwordConfirm, setPasswordConfirm] = useState<string>('');
     const [isModal, setIsModal] = useState<boolean>(false);
     const [modalTxt, setModalTxt] = useState<string>('');
+    const [emailValidateMsg, setEmailValidateMsg] = useState<string>('');
+    const [passwordValidateMsg, setPasswordValidateMsg] = useState<string>('')
 
 
     useEffect(() => {
@@ -40,8 +45,26 @@ const Home: NextPage = () => {
     };
 
 
-    const formSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    const passwordConfirmChangeHandler = (e: ChangeEvent) => {
+        const target = e.target as HTMLInputElement;
+        setPasswordConfirm(target.value);
+    };
+
+    const validate = () => {
+        if (!validateEmail(email)) {
+            setEmailValidateMsg('올바른 형식의 이메일을 입력해주세요');
+            return false;
+        } else if (!validatePassword(password))  {
+            setPasswordValidateMsg('패스워드는 영문자, 숫자가 포함된 8 이상의 글자입니다');
+            return false;
+        }
+    }
+
+    const loginSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        if (!validate()) return;
+
         signInWithEmailAndPassword(auth, email, password)
             .then(({user}) => {
                 user.getIdToken().then(token => {
@@ -61,19 +84,75 @@ const Home: NextPage = () => {
                 }
                 setIsModal(true);
             })
+
+        setEmail('');
+        setPassword('');
+    };
+
+
+    const signUpSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!validate()) return;
+
+        if (password !== passwordConfirm) {
+            setModalTxt('패스워드와 패스워드 확인을 동일하게 입력해주세요');
+            setIsModal(true);
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                setModalTxt('회원가입에 성공하였습니다.');
+                setIsLoginForm(true);
+            })
+            .catch(console.log);
+
+        setIsModal(true);
+        setEmail('');
+        setPassword('');
+        setPasswordConfirm('');
+    };
+
+
+    const toggleForm = (isLoginForm: boolean) => {
+        setIsLoginForm(isLoginForm);
+        setEmail('');
+        setPassword('');
+        setPasswordConfirm('');
+        setEmailValidateMsg('');
+        setPasswordValidateMsg('');
     };
 
 
     return (
         <>
-            <form onSubmit={formSubmitHandler}
-                  className='w-full flex flex-col justify-center items-center gap-4'>
-                <TextInput onChange={emailChangeHandler} placeHolder='이메일을 입력해주세요.' labelTxt='이메일' inputType='email'/>
-                <TextInput onChange={passwordChangeHandler } placeHolder='패스워드를 입력해주세요.' labelTxt='패스워드' inputType='password'/>
-                <div className="actions">
-                    <input type='submit' className='btn btn-wide mt-4' />
-                </div>
-            </form >
+            {isLoginForm &&
+                <form onSubmit={loginSubmitHandler}
+                      className='w-full flex flex-col justify-center items-center gap-6'>
+                  <TextInput validateMsg={emailValidateMsg} onChange={emailChangeHandler} placeHolder='이메일을 입력해주세요.' labelTxt='이메일' inputType='email'/>
+                  <TextInput validateMsg={passwordValidateMsg} onChange={passwordChangeHandler } placeHolder='영문자, 숫자가 포함된 6~17 글자 입력' labelTxt='패스워드' inputType='password'/>
+                  <div className="actions">
+                    <input type='submit' value='로그인' className='btn btn-wide mt-10' />
+                  </div>
+                  <div onClick={() => setIsLoginForm(false)}
+                       className='text-gray-500 text-sm cursor-pointer'>회원가입</div>
+                </form >
+            }
+
+            {!isLoginForm &&
+                <form onSubmit={signUpSubmitHandler}
+                    className='w-full flex flex-col justify-center items-center gap-6'>
+                  <TextInput validateMsg={emailValidateMsg} onChange={emailChangeHandler} placeHolder='이메일을 입력해주세요.' labelTxt='이메일' inputType='email'/>
+                  <TextInput validateMsg={passwordValidateMsg} onChange={passwordChangeHandler} placeHolder='영문자, 숫자가 포함된 6~17 글자의 숫자를 입력해주세요' labelTxt='패스워드' inputType='password'/>
+                  <TextInput onChange={passwordConfirmChangeHandler} placeHolder='패스워드를 입력해주세요.' labelTxt='패스워드 확인' inputType='password'/>
+                  <div className="actions">
+                    <input type='submit' value='회원가입' className='btn btn-wide mt-10' />
+                  </div>
+                  <div onClick={() => toggleForm(true)}
+                       className='text-gray-500 text-sm cursor-pointer'>로그인</div>
+                </form>
+            }
 
             <InformModal
                 isShow={isModal}
