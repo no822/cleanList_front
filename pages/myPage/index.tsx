b8b7f1011beeb3594b3883c17b37c062aac3e725
no822 <nolan.kim@cobosys.co.kr>
2023-03-22 05:36:43 +0900

    마이페이지 아이콘 추가

--- pages/myPage/index.tsx
+++ pages/myPage/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {useAppDispatch} from "../../store/hooks";
 import {authAction} from "../../store/authSlice";
 import {deleteToken} from "../../utils/token";
 import {useRouter} from "next/router";
 import {signOut} from "@firebase/auth";
-import auth from '../../network/auth';
+import auth from "../../network/auth";
+import {MdLogout, MdLibraryBooks} from "react-icons/md";
 
 const Index = () => {
     const dispatch = useAppDispatch();
     const router = useRouter();
 
     const logoutHandler = () => {
-        // 리덕스
         signOut(auth).then(_ => {
             dispatch(authAction.logout());
             deleteToken();
@@ -19,15 +19,25 @@ const Index = () => {
         });
     };
 
-    return <>
+    return (
         <ul className="menu  w-full p-5 shadow bg-base-100 mb-6 rounded-box">
-            <li><a className='active:bg-blue-500'>청소기록</a></li>
+            <li>
+                <a className='active:bg-blue-500'>
+                    <MdLibraryBooks color='black' size='2rem' />
+                    청소기록
+                </a>
+            </li>
             <div className="divider my-2"></div>
 
-            <li onClick={logoutHandler}><a className='active:bg-blue-500'>로그아웃</a></li>
+            <li onClick={logoutHandler}>
+                <a className='active:bg-blue-500'>
+                    <MdLogout color='black' size='2rem' />
+                    로그아웃
+                </a>
+            </li>
             <div className="divider my-2"></div>
         </ul>
-    </>
+    );
 };
 
 export default Index;
